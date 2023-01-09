39ffa565f12a68f0a4656bf97fd56bbe3d485f55
mqsoares <mqseraos@gmail.com>
2023-01-08 21:42:23 -0300

    Create component Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+    render() {
+        return(
+            <button>Abrir Biscoito</button>
+        );
+    }
+}
+
+export default Button;
