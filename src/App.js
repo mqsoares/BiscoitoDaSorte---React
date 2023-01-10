c5ff396fe234aed9644318e6199c53716b3dd5df
mqsoares <mqseraos@gmail.com>
2023-01-09 21:22:34 -0300

    Create App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import imgBiscuit from './assets/biscuit.svg'
+// import imgBrokenBiscuit from './assets/brokenBiscuit.svg'
+import Button from './components/Button';
 
 function App() {
   return (
-    <>
-    </>
+    <div className='container'>
+      <h1>Biscoito da Sorte</h1>
+    
+      <p>Ao abrir um biscoito da sorte nos deparamos com uma sábia mensagem de motivação. As mensagens originam-se de estudos da sabedoria chinesa sobre o autoconhecimento.</p> 
+
+      <img src={imgBiscuit} alt="Imagem do biscoito da sorte"/>
+      
+      <Button />
+    </div>
   );
 }
 
