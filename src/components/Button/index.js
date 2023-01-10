54a4b589c40f34b5150a114aa2505c3b500f04ed
mqsoares <mqseraos@gmail.com>
2023-01-10 12:23:41 -0300

    Create onClick Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Button = ({children, className}) => {
+const Button = ({children, className, onClick}) => {
     return(
-        <button className={className}>{children}</button>
+        <button className={className} onClick={onClick}>{children}</button>
     )
 }
 
